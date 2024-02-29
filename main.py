328c3b000b6275b66c5a50aa9e798584307c5c6a
CodeBlueJay <126738791+CodeBlueJay@users.noreply.github.com>
2024-02-29 00:23:04 +0000

    Started the game

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+import time
 
+commands = ["north", "east", "south", "west", "map", "use", "grab", "inventory", "stats"]
+
+def show_map():
+    print("____________________________________________________________________________")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|                                                                          |")
+    print("|__________________________________________________________________________|")
+
+def welcome_message():
+    print(" _    _      _                            _          _____         _      ___      _                 _                  _ ")
+    print("| |  | |    | |                          | |        |_   _|       | |    / _ \    | |               | |                | |")
+    print("| |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___     | | _____  _| |_  / /_\ \ __| |_   _____ _ __ | |_ _   _ _ __ ___| |")
+    print("| |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \    | |/ _ \ \/ / __| |  _  |/ _` \ \ / / _ \ '_ \| __| | | | '__/ _ \ |")
+    print("\  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |   | |  __/>  <| |_  | | | | (_| |\ V /  __/ | | | |_| |_| | | |  __/_|")
+    print(" \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/    \_/\___/_/\_\\__| \_| |_/\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___(_)")
+
+def start_game():
+    welcome_message()
+    print("You are a lone Troy Student, fighting to secure good grades and making your Asian parents proud.")
+    print("Commands: " + str(commands))
+    print()
+    time.sleep(1)
+    print("You stand in front of the North Gym gate.")
+    print("You have a map of the school")
+    show_map()
+
+start_game()
